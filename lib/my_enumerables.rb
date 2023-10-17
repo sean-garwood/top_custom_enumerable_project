f83a912fcfcfb47d17556e756d9716bc3bc347f2
sean-garwood <seansgarwood@gmaill.com>
2023-10-16 22:00:19 -0400

    chore: tests
    Pass spec/my_each_with_index_spec.rb
    
    Pass spec/my_select_spec.rb
    
    Pass spec/my_all_spec.rb
    
     Pass spec/my_any_spec.rb
    
     Pass spec/my_none_spec.rb
    
     Pass spec/my_count_spec.rb
    
    Pass spec/my_map_spec.rb
    
    Pass all tests, assignment completed.
    
    Clean up
    
    Clean up Array#my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,57 @@
+# frozen_string_literal: true
+
+# custom enumberable methods, included in Array class
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    my_each { |e| yield(e, i) }; i+=1
+    self
+  end
+
+  def my_count(&condition)
+    !block_given? && length || my_select do |e|
+      condition.call(e).length
+    end
+  end
+
+  def my_inject(accumulator, &block)
+    my_each do |e|
+      block.call(accumulator, e) => accumulator
+    end
+    accumulator
+  end
+
+  def my_map
+    mapped = []
+    my_each { |e| mapped << yield(e) }
+    mapped
+  end
+
+  def my_select(&condition)
+    selected = []
+    my_each { |e| selected << e if condition.call(e) }
+    selected
+  end
+
+  def my_all?(&condition)
+    self == my_select { |e| condition.call(e) }
+  end
+
+  def my_any?(&condition)
+    !my_select { |e| condition.call(e) }.empty?
+  end
+
+  def my_none?(&condition)
+    !my_any? { |e| condition.call(e) }
+  end
 end
 
+# Rebuild the Array#each method, use it as basis for fns in module Enumberable
 class Array
+include Enumerable
   def my_each
-    for i in 0..self.length-1
-      yield(self[i])
+    for e in self
+      yield e
     end
     self
   end
